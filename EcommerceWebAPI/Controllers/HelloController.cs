fe5857de701e437d2eddce147d8a39dc477fd3aa
Azhar786786786 <talkazhar786@hotmail.com>
2026-06-18 12:20:28 +0530

    Added new Controller and changes configuration

---
+++ EcommerceWebAPI/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Message()
+        {
+            return Ok("This is first message from my side.");
+        }
+    }
+}
